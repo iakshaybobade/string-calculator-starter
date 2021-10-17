28d2890823c5367b0379f57f325cca2db30848aa
Akshay Bobade <iakshaybobade@gmail.com>
2021-10-17 19:51:21 +0530

    Testcase 1 multiple numbers pass

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,15 +3,22 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	String[] numbers=input.split(",");
     	if(input.isEmpty()) {
     		return 0;
     	}
-    	else {
+    	if(input.length()==1) {
     		return stringToInt(input);
+    	}
+    	else {
+    		return stringToInt(numbers[0])+stringToInt(numbers[1]);
     	}
     	
     	
-    } private int stringToInt(String input) {
+    } 
+    
+    
+    private int stringToInt(String input) {
 		return Integer.parseInt(input);
     	
     }
