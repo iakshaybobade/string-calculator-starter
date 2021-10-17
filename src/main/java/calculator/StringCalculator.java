7d66f13f9f0224c43301d96d964f09c7baaa9da5
Akshay Bobade <iakshaybobade@gmail.com>
2021-10-17 20:02:06 +0530

    Testcase 1 multiple numbers reutn add Refactor

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,35 +2,34 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-    	String[] numbers=input.split(",");
-    	if(isEmpty(input)) {
-    		return 0;
-    	}
-    	if(input.length()==1) {
-    		return stringToInt(input);
-    	}
-    	else {
-    		return getsum(numbers);
-    	}
-    	
-    	
-    } 
-    
-    private boolean isEmpty(String input) {
-    	return input.isEmpty()
-;    }
-    private int getsum(String[] num) {
-    	int sum=0;
-    	for(int i=0;i<num.length;i++) {
-    		sum=sum+Integer.parseInt(num[i]);
-    	}
-    	return sum;
-    }
-    private int stringToInt(String input) {
-		return Integer.parseInt(input);
-    	
-    }
-   
+	public int add(String input) {
+		String[] numbers = input.split(",");
+		if (isEmpty(input)) {
+			return 0;
+		}
+		if (input.length() == 1) {
+			return stringToInt(input);
+		} else {
+			return getsum(numbers);
+		}
 
-} 
+	}
+
+	private boolean isEmpty(String input) {
+		return input.isEmpty();
+	}
+
+	private int getsum(String[] num) {
+		int sum = 0;
+		for (String i:num) {
+			sum = sum + stringToInt(i);
+		}
+		return sum;
+	}
+
+	private int stringToInt(String input) {
+		return Integer.parseInt(input);
+
+	}
+
+}
